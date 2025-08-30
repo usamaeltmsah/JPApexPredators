b2f9d8aa18b82e404c2e78369ae52c2cbccb5d15
Usama Fouad <usamaeltmsah@gmail.com>
2025-08-31 00:38:20 +0300

    navigate to PredatorMap with navigation transition

--- JPApexPredators/PredatorDetail.swift
+++ JPApexPredators/PredatorDetail.swift
@@ -12,6 +12,8 @@ struct PredatorDetail: View {
     let predator: ApexPredator
     @State var position: MapCameraPosition
     
+    @Namespace var namespace
+    
     var body: some View {
         GeometryReader { geo in
             ScrollView {
@@ -41,9 +43,14 @@ struct PredatorDetail: View {
                     
                     // Current location
                     NavigationLink {
-                        Image(predator.image)
-                            .resizable()
-                            .scaledToFit()
+                        PredatorMap(position: .camera(
+                            MapCamera(
+                                centerCoordinate: predator.location,
+                                distance: 1000,
+                                heading: 250,
+                                pitch: 80
+                            )))
+                        .navigationTransition(.zoom(sourceID: 1, in: namespace))
                     } label: {
                         Map(position: $position) {
                             Annotation(predator.name, coordinate: predator.location) {
@@ -70,6 +77,7 @@ struct PredatorDetail: View {
                             .background(.black.opacity(0.33))
                             .clipShape(.rect(bottomTrailingRadius: 15))
                     }
+                    .matchedTransitionSource(id: 1, in: namespace)
                     
                     // Appears in
                     Text("Appears In:")

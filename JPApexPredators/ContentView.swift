51022ff3e38e5e6a9111cb22430e2f141cb1a46f
Usama Fouad <usamaeltmsah@gmail.com>
2025-08-30 14:30:31 +0300

    implement the ui

--- JPApexPredators/ContentView.swift
+++ JPApexPredators/ContentView.swift
@@ -8,14 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    var filteredDinos: [ApexPredator] {
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List(filteredDinos) { predator in
+                NavigationLink {
+                    Image(predator.image)
+                        .resizable()
+                        .scaledToFit()
+                } label: {
+                    HStack {
+                        // Dinasour image
+                        Image(predator.image)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 100, height: 100)
+                            .shadow(color: .white, radius: 1)
+                        VStack(alignment: .leading) {
+                            // Name
+                            Text(predator.name)
+                                .fontWeight(.bold)
+                            
+                            // Type
+                            Text(predator.type.rawValue.capitalized)
+                                .font(.subheadline)
+                                .fontWeight(.semibold)
+                                .padding(.horizontal, 13)
+                                .padding(.vertical, 5)
+                                .background(predator.type.background)
+                                .clipShape(.capsule)
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Apex Predators")
+            .autocorrectionDisabled()
+            .animation(.default, value: searchText)
         }
-        .padding()
+        .preferredColorScheme(.dark)
     }
 }
 
